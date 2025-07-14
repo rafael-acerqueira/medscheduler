f5c525e9b5118fd5d3e658b2da8f685163a56c19
Rafael Aquino <rafael.acerqueirah@gmail.com>
2025-07-14 09:22:00 -0300

    Create User

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ADMIN = 'admin'
+    DOCTOR = 'doctor'
+    PATIENT = 'patient'
+
+    ROLE_CHOICES = [
+        (ADMIN, 'Admin'),
+        (DOCTOR, 'Doctor'),
+        (PATIENT, 'Patient'),
+    ]
+
+    role = models.CharField(
+        max_length=10,
+        choices=ROLE_CHOICES,
+        default=PATIENT,
+        help_text='Role of the user: admin, doctor or patient'
+    )
+
+
